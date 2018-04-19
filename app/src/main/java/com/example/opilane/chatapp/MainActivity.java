3da920cc0e1175e0f0425e7b19cd2272afbbf1f9
opilane <heigopaevere@gmail.com>
2018-04-19 10:06:02 +0300

    ___

--- app/src/main/java/com/example/opilane/chatapp/MainActivity.java
+++ app/src/main/java/com/example/opilane/chatapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.opilane.chatapp;
 
 import android.content.Intent;
+import android.renderscript.ScriptGroup;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,11 +35,12 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
                     Snackbar.make(activity_main, "You have been signed out.",Snackbar.LENGTH_SHORT).show();
+                    finish();
 
                 }
             });
         }
-        return item;
+        return true;
     }
 
     @Override
@@ -41,7 +48,12 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SIGN_IN_REQUEST_CODE){
             if (requestCode == RESULT_OK){
-                Snackbar.make(activity_main,"Succsesfully signed in. Welcome!")
+                Snackbar.make(activity_main,"Succsesfully signed in. Welcome!", Snackbar.LENGTH_SHORT).show();
+                displayChatMessage();
+            }
+            else {
+                Snackbar.make(activity_main,"We couldn't sign you in. Please try again later.",Snackbar.LENGTH_SHORT);
+                finish();
             }
         }
 
@@ -57,12 +69,41 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                EditText input = (EditText)findViewById(R.id.input);
+                FirebaseDatabase.getInstance().getReference().push()
+                        .setValue(new ChatMessage(input.getText().toString(), FirebaseAuth.getInstance()
+                        .getCurrentUser().getEmail()));
+                        input.setText("");
             }
         });
+        if (FirebaseAuth.getInstance().getCurrentUser()==null){
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),SIGN_IN_REQUEST_CODE);
+        }
+        else {
+            Snackbar.make(activity_main,"Welcome " +FirebaseAuth.getInstance().getCurrentUser().
+            getEmail(),Snackbar.LENGTH_SHORT).show();
+            displayChatMessage();
+        }
     }
 
     private void displayChatMessage(){
+        ListView ListOfMessage = (ListView) findViewById(R.id.list_of_message);
+        adapter = new FirebaseListAdapter<ChatMessage>(this,ChatMessage.class,R.layout.list_item,
+                FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                TextView messageText,messageUser,messageTime;
+                messageText = findViewById(R.id.message_text);
+                messageTime = findViewById(R.id.message_time);
+                messageUser = findViewById(R.id.message_user);
+
+                messageText.setText(model.getMessageText());
+                messageTime.setText(android.text.format.DateFormat.format
+                        ("dd-MM-yyyy(HH:mm:ss)",model.getMessageTime()));
+                messageUser.setText(model.getMessageUser());
+            }
+        };
+        ListOfMessage.setAdapter(adapter);
     }
 }
 
